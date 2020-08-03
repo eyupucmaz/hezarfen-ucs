f17c2613c24db5f954031d38dfa8fa39f71f39d8
eyupucmaz <eyupucmaz@gmail.com>
2020-08-03 20:56:50 +0300

    Started project.

---
+++ src/components/CustomNavItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const CustomNavItem = (props) => {
+	return (
+		<div className="nav-item">
+			<Button>Click Me!</Button>
+		</div>
+	);
+};
+
+export { CustomNavItem };
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { CustomNavItem } from "./";
+export default class Main extends Component {
+	render() {
+		return (
+			<div>
+				<CustomNavItem></CustomNavItem>
+			</div>
+		);
+	}
+}
