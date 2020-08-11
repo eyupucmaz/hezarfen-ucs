e58ceee391220d230e5ab8ab785e9611bc2c36e1
eyupucmaz <eyupucmaz@gmail.com>
2020-08-11 23:34:38 +0300

    add some route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Login, Home, Signup } from "./components";
+import { Login, Home, Signup, ForgotPassword, Dashboard } from "./components";
 import { Container } from "react-bootstrap";
 import fire from "./firebase";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -34,9 +34,11 @@ export default class App extends Component {
 			<Router>
 				<Container fluid className="p-0 vh-100 vw-100">
 					<Switch>
-						<Route path="/" exact component={Home} />
-						<Route path="/signin" component={Login} />
-						<Route path="/signup" component={Signup} />
+						<Route exact path="/" component={Home} />
+						<Route exact path="/signin" component={Login} />
+						<Route exact path="/signup" component={Signup} />
+						<Route exact path="/forgot-password" component={ForgotPassword} />
+						<Route exact path="/dashboard" component={Dashboard} />
 					</Switch>
 				</Container>
 			</Router>
