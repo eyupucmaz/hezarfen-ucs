3dcdd95310441fe701e3a8d10f0dbb6ccdc94245
eyupucmaz <eyupucmaz@gmail.com>
2020-08-11 23:36:40 +0300

    edit login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
-import { Container, Image, Form, Button, NavLink } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Image, Form, Button, Alert } from "react-bootstrap";
 import "./Login.css";
 import fire from "../firebase";
 import { Link } from "react-router-dom";
+import alertify from "alertifyjs";
 
-class Login extends Component {
-	login() {
+const Login = () => {
+	const [isLogin, setIsLogin] = useState();
+
+	const login = (e) => {
 		const email = document.querySelector("#email").value;
 		const password = document.querySelector("#password").value;
 
@@ -13,80 +16,71 @@ class Login extends Component {
 			.auth()
 			.signInWithEmailAndPassword(email, password)
 			.then((u) => {
-				console.log("You are inn");
+				setIsLogin(true);
+				alertify.set("notifier", "position", "top-center");
+				alertify.success("Giris Yaptiniz", 4);
 			})
 			.catch((err) => {
-				console.log("Error:" + err.toString());
+				alertify.set("notifier", "position", "top-center");
+				alertify.error(err.toString(), 4);
 			});
-	}
-	signUp() {
-		const email = document.querySelector("#email").value;
-		const password = document.querySelector("#password").value;
+		e.preventDefault();
+	};
 
-		fire
-			.auth()
-			.createUserWithEmailAndPassword(email, password)
-			.then((u) => {
-				console.log("You are inn");
-			})
-			.catch((err) => {
-				console.log("Error:" + err.toString());
-			});
-	}
-
-	render() {
-		return (
-			<Container
-				fluid
-				className="container-login d-flex justify-content-center align-items-center vh-100 vw-100"
-			>
-				<div className="login-form d-flex flex-column justify-content-center align-items-center">
-					{/* LOGO END TEXT */}
+	return (
+		<Container
+			fluid
+			className="container-login d-flex justify-content-center align-items-center vh-100 vw-100"
+		>
+			<div className="login-form d-flex flex-column justify-content-center align-items-center">
+				{/* LOGO END TEXT */}
+				<Link to="/">
 					<Image
 						className="logo img-fluid"
 						src={require("../image/logo-text.svg")}
 					/>
-					<h1 className="login-text text-white mt-3 mb-3">Giriş Yap</h1>
-					{/* LOGIN FORM */}
-					<Form className="w-100">
-						<Form.Group className="mt-5">
-							<Form.Control id="email" type="email" placeholder="E-mail" />
-						</Form.Group>
+				</Link>
+				<h1 className="login-text text-white mt-3 mb-3">Giriş Yap</h1>
+				{/* LOGIN FORM */}
+				<Form className="w-100" onSubmit={login}>
+					<Form.Group className="mt-5">
+						<Form.Control
+							id="email"
+							type="email"
+							placeholder="E-mail"
+							required
+						/>
+					</Form.Group>
 
-						<Form.Group className="mt-4">
-							<Form.Control
-								id="password"
-								type="password"
-								placeholder="Parola"
-							/>
-						</Form.Group>
+					<Form.Group className="mt-4">
+						<Form.Control
+							id="password"
+							type="password"
+							placeholder="Parola"
+							required
+						/>
+					</Form.Group>
 
-						<Button
-							className="w-100 mt-4 login-button"
-							variant="primary"
-							onClick={this.login}
-						>
-							Giriş Yap
-						</Button>
-					</Form>
-					<div className="login-footer">
-						<NavLink className="forget-password float-left p-0" href="#">
-							Parolami Unutum
-						</NavLink>
-						<Link to="/signup">
-							<NavLink
-								onClick={this.signUp}
-								className="signup float-right p-0"
-								href="#"
-							>
-								Kayıt Ol
-							</NavLink>
-						</Link>
-					</div>
+					<Button
+						type="submit"
+						className="w-100 mt-4 login-button"
+						variant="primary"
+					>
+						Giriş Yap
+					</Button>
+				</Form>
+
+				<div className="login-footer">
+					<Link to="/forgot-password">
+						<p className="forgot-password float-left p-0">Parolami Unutum</p>
+					</Link>
+					<Link to="/signup">
+						<p className="signup float-right p-0">Kayıt Ol</p>
+					</Link>
 				</div>
-			</Container>
-		);
-	}
-}
+			</div>
+		</Container>
+	);
+};
 
 export { Login };
